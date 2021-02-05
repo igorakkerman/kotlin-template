17927745751c78d343fa6a0623bb63f55d983183
Igor Akkerman <commit@igorakkerman.de>
2021-02-06 00:38:04 +0100

    Project dependencies upgraded

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.3.61"
+    kotlin("jvm") version "1.4.30"
 }
 
 repositories {
@@ -9,10 +9,11 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.2.0")
-    testImplementation("ch.tutteli.atrium:atrium-fluent-en_GB:0.9.0-alpha")
-    testRuntimeOnly("net.bytebuddy:byte-buddy:1.10.6")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    testImplementation("ch.tutteli.atrium:atrium-fluent-en_GB:0.15.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-reflect:1.4.30")
+    testRuntimeOnly("net.bytebuddy:byte-buddy:1.10.20")
 }
 
 tasks.test {
