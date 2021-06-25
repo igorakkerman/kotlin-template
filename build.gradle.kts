e5cb01733ed2edec377aecd8899ef5730c021e48
Igor Akkerman <commit@igorakkerman.de>
2021-06-25 20:22:38 +0200

    Test libraries upgraded

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
-    testImplementation ("io.kotest:kotest-assertions-core-jvm:4.4.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testImplementation ("io.kotest:kotest-assertions-core-jvm:4.6.0")
 }
 
 tasks.test {
